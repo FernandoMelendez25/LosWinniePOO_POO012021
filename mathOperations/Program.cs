8bfa18e5cdf1a2e607b2c496af95f1f479bd29ec
FernandoMelendez25 <00018720@uca.edu.sv>
2021-03-14 17:59:00 -0600

    Modify: Substract to MultiplyNumbers

--- mathOperations/Program.cs
+++ mathOperations/Program.cs
@@ -14,11 +14,11 @@ namespace mathOperations
             Console.WriteLine("Enter a value for b: ");
             b = Int32.Parse(Console.ReadLine());
             
-            Console.WriteLine($"Subtracting {a} - {b} equals {SubtractNumbers(a,b)}");
+            Console.WriteLine($"Multiplying {a} x {b} equals {MultiplyNumbers(a,b)}");
         }
-        static int SubtractNumbers(int a, int b)
+        static int MultiplyNumbers(int a, int b)
         {
-            return a - b;
+            return a * b;
         }
     }
 }
